fcd72e6590f76765542fa9a259aa3e040d27d379
kemblesong <kemblesong@gmail.com>
2018-06-11 18:09:51 +1000

    Revamp

--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import Rellax from 'rellax'
 
-// Your top level component
-import App from './App'
-
-// Export your top level component as JSX (for static rendering)
-export default App
-
-// Render your app
-if (typeof document !== 'undefined') {
-  const renderMethod = module.hot ? ReactDOM.render : ReactDOM.hydrate || ReactDOM.render
-  const render = Comp => {
-    renderMethod(<Comp />, document.getElementById('root'))
-  }
-
-  // Render!
-  render(App)
-}
+window.rellax = new Rellax('.rellax')
